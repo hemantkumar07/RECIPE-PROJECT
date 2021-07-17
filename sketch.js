ba89b941c376cb1a1df69e319a4267eda955c7ae
HEMANT_SINHA <79569929+hemantkumar07@users.noreply.github.com>
2021-07-17 13:53:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+TEA RECIPE
+Ingredients -
+sugar,
+milk,
+water,
+ginger,
+tea leaves
+HOW TO MAKE TEA -
+take 1/2 cup of water in a bowl,
+then add 1 table spoon of tea leaves,
+then add 1 table spoon of sugar,
+add 1 piece of ginger grated,
+put the mixture on gas and let it boil,
+once you see the water boiling,
+add 1 cup milk,
+and let it boil till the colour of the liquid becomes brown
+(you can also add tea masala or cardamom if you like while boiling the water)
